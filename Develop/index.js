05119e529f1a5a3f49b353aa870bce04a1a4bef6
tmerritt56 <104723943+tmerritt56@users.noreply.github.com>
2022-08-11 22:00:35 -0500

    added code for readme file

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,8 @@
 // TODO: Include packages needed for this application
+const { table } = require('console');
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [];
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  // need to add license badge codes.
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -14,7 +16,35 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  ## Description:
+  ${data.description}
+  
+  ## Table of Contents:
+  -[Installation](#installation)
+  -[Usage](#usage)
+  -[Contributing](#contributing)
+  -[Test](#test)
+  -[Questions](#questions)
+
+
+  ## Installation:
+  ${data.installation}
+  
+  ## Usage:
+  ${data.usage}
+  
+  ## Contributing:
+  ${data.contributing}
+  
+  ## Tests:
+  ${data.tests}
+  
+  ## License:
+  ${data.license}
+  
+  ### Questions:
+  ${data.questions}`
+;
 }
 
 module.exports = generateMarkdown;
